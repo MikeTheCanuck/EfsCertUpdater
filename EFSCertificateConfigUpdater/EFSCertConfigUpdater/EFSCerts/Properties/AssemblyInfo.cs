9badb5bcded8529eae699cfd9f2a304715fb1cd7
Mike Lonergan <MikeTheCanuck@users.noreply.github.com>
2007-10-26 16:12:18 +0000

    Added the Windows Installer project to the solution, and made a few minor configuration changes to the application's project.  This is the version of the code that was shipped in version 1.1.

--- EFSCertificateConfigUpdater/EFSCertConfigUpdater/EFSCerts/Properties/AssemblyInfo.cs
+++ EFSCertificateConfigUpdater/EFSCertConfigUpdater/EFSCerts/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EFSConfiguration")]
+[assembly: AssemblyTitle("EFS Certificate Configuration Updater")]
 [assembly: AssemblyDescription("Examines and updates the user's active EFS digital certificate")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EFSConfiguration")]
+[assembly: AssemblyProduct("EFS Certificate Configuration Updater")]
 [assembly: AssemblyCopyright("Copyright © Mike Smith-Lonergan 2007")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
