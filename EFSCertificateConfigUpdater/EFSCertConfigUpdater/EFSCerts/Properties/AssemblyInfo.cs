799fec4459b064792498bab25f180f53e93de376
Mike Lonergan <MikeTheCanuck@users.noreply.github.com>
2008-11-28 03:43:11 +0000

    Adding back correctly-referenced source files.

---
+++ EFSCertificateConfigUpdater/EFSCertConfigUpdater/EFSCerts/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EFS Certificate Configuration Updater")]
+[assembly: AssemblyDescription("Examines and updates the user's active EFS digital certificate")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EFS Certificate Configuration Updater")]
+[assembly: AssemblyCopyright("Copyright © Mike Smith-Lonergan 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f4117660-f7c4-432b-96e6-99964c377b5e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
